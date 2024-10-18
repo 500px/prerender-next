af71bf21393f3ad739d41a21955eb7247bffa76c
jessica-liyan <277948745@qq.com>
2024-10-18 14:41:15 +0800

    add log

--- app/photo/[id]/page.tsx
+++ app/photo/[id]/page.tsx
@@ -14,6 +14,7 @@ export async function generateMetadata(
   },
   parent: ResolvingMetadata
 ): Promise<Metadata> {
+  console.log('coming', params.id, searchParams.flag);
   const flag = searchParams?.flag === "1";
   const { photo } = await graphqlQuery(flag ? photoAwardQuery : photoQuery, {
     photoLegacyId: params.id,
@@ -96,16 +97,16 @@ const PhotoDetail = async ({
   searchParams: { flag: string };
 }) => {
   // 以下用于测试生成的预览图，后续删除
-  // const { photo } = await graphqlQuery(photoAwardQuery, {
-  //   photoLegacyId: params.id,
-  //   resourceType: "Photo",
-  // });
-  // const url = await generatePreviewImage(photo, true);
-  // return (
-  //   <>
-  //     <img src={url} alt={photo.name} />
-  //   </>
-  // );
+  const { photo } = await graphqlQuery(photoAwardQuery, {
+    photoLegacyId: params.id,
+    resourceType: "Photo",
+  });
+  const url = await generatePreviewImage(photo, true);
+  return (
+    <>
+      <img src={url} alt={photo.name} />
+    </>
+  );
   return <></>;
 };
 
